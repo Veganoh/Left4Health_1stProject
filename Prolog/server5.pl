b656506aca431fe14799e1ef6ccb7243cb0e74c3
Mariana Ribeiro <marianadsr.2001@gmail.com>
2023-11-03 21:04:56 +0000

    organize code

--- Prolog/server5.pl
+++ Prolog/server5.pl
@@ -8,26 +8,23 @@
 servidor(Porta) :-
     http_server(http_dispatch, [port(Porta)]).
 
-:- http_handler('/api/quizInitial', obter_ansiedade, [method(post)]).
-obter_ansiedade(Request) :-
+:- http_handler('/api/quizInitial', quizInitial, [method(post)]).
+quizInitial(Request) :-
     member(method(post), Request),
     http_read_data(Request, Data, [to(string)]),
     ContadorInicial = 1, % Inicialize ContadorInicial com o valor desejado
     processar_corpo(Data, Resultado, ContadorInicial, ContadorFinal),
     format('Content-type: text/plain; charset=UTF-8~n~n'),
-    format('Post recebido com sucesso~n'),
-    format('Numero de fatos criados: ~d~n', [ContadorFinal]),
+    format('Numero de fatos criados: ~d~n', [ContadorFinal-1]),
     format('~w', [Resultado]). % Saída em texto simples
 
 
 :- servidor(8080).
 
 processar_corpo(Dados, Resultado, Contador, ContadorFinal) :-
-    %Linhas= Dados.split('\r\n'),
     split_string(Dados, "\r\n", "\r\n", Linhas),
-    remover_espacos_e_linhas(Linhas, ListaSemEspacos),
-    split_into_pairs(ListaSemEspacos, Pares),
-    processar_pares(Pares, FactosCriados, Contador, ContadorFinal),
+    split_into_pairs(Linhas, Pares),
+    create_facts(Pares, FactosCriados, Contador, ContadorFinal),
     Resultado = FactosCriados.
 
 split_into_pairs([], []).
@@ -35,26 +32,14 @@ split_into_pairs([Number, Answer | Rest], [[Number, Answer] | Pairs]) :-
     split_into_pairs(Rest, Pairs).
 
 
-processar_pares([], [], Contador, Contador).
-processar_pares([[Pergunta, Resposta] | RestoLinhas], [pergunta(Pergunta, Resposta) | RestoPares], Contador, ContadorFinal) :-
+create_facts([], [], Contador, Contador).
+create_facts([[Pergunta, Resposta] | RestoLinhas], [pergunta(Pergunta, Resposta) | RestoPares], Contador, ContadorFinal) :-
     atom_codes(AtomPergunta, Pergunta),
     atom_number(AtomPergunta, PeguntaNumber),
     atom_codes(AtomResposta, Resposta),
     assertz(facto(Contador, pergunta(PeguntaNumber, AtomResposta))),
     NewContadorFactos is Contador + 1,
-    processar_pares(RestoLinhas, RestoPares, NewContadorFactos, ContadorFinal).
+    create_facts(RestoLinhas, RestoPares, NewContadorFactos, ContadorFinal).
 
 
 
-
-remover_espacos_e_linhas([], []).
-remover_espacos_e_linhas([Espaco | Resto], SemEspacos) :-
-    Espaco = ' ', % Verifica se o elemento é um espaço em branco
-    remover_espacos_e_linhas(Resto, SemEspacos).
-remover_espacos_e_linhas([LinhaEmBranco | Resto], SemEspacos) :-
-    LinhaEmBranco = '', % Verifica se o elemento é uma linha em branco
-    remover_espacos_e_linhas(Resto, SemEspacos).
-remover_espacos_e_linhas([Elemento | Resto], [Elemento | SemEspacos]) :-
-    Elemento \= ' ', % Se o elemento não for um espaço em branco
-    Elemento \= '',  % E não for uma linha em branco
-    remover_espacos_e_linhas(Resto, SemEspacos).
--- Prolog/test/motorInferencia.pl
+++
@@ -1,4 +0,0 @@
-% Defina o motor de inferência aqui
-realizar_inferencia :-
-    % Coloque seu código de inferência aqui
-    writeln('Motor de inferencia executado com sucesso.').
