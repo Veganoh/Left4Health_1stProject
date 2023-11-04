f5af71822442074ecb337af5cb8a2eef0f5c0fde
LeoJJoao <leonardoguerreirojoao@gmail.com>
2023-11-04 17:09:37 +0000

    Update para quiz40
    Enviar e receber as 40 perguntas

--- Prolog/test/server.pl
+++ Prolog/test/server.pl
@@ -31,16 +31,6 @@ get_perguntas_iniciais(Request) :-
     format('Content-type: text/plain~n~n'),
     format('~s', [Response]).
 
-% Define a handler for the /api/quiz40 endpoint
-:- http_handler('/api/quiz40', get_perguntas_40, [method(get)]).
-% Handler for the GET request to /api/quiz40 endpoint
-get_perguntas_40(Request) :-
-    perguntas_misturadas(Perguntas),
-    format_response(Perguntas, Response),
-    format('Content-type: text/plain~n~n'),
-    format('~s', [Response]).
-
-
 % HTTP POST para receber as respostas do quizInitial
 :- http_handler('/api/answersQuizInitial', quizInitial, [method(post)]).
 quizInitial(Request) :-
@@ -55,6 +45,98 @@ quizInitial(Request) :-
     format('Content-type: text/plain; charset=UTF-8~n~n'),
     format('Numero de fatos criados: ~d~n', [ValorUltimoFacto]),
     format('~w', [Resultado]). % Saída em texto simples
+
+% Define a handler for the /api/quiz40 endpoint
+:- http_handler('/api/quiz40', get_perguntas_40, [method(get)]).
+% Handler for the GET request to /api/quiz40 endpoint
+get_perguntas_40(Request) :-
+    perguntas_misturadas(Perguntas),
+    format_response(Perguntas, Response),
+    format('Content-type: text/plain~n~n'),
+    format('~s', [Response]).
+
+% Define a handler for the /answersQuiz40 endpoint
+%:- http_handler('/api/answersQuiz40', answers_quiz40_handler, [methods([post])]).
+
+% Handler for the POST request to /answersQuiz40 endpoint
+%answers_quiz40_handler(Request) :-
+%    http_read_data(Request, Data, []),
+%    process_input(Data, Response),
+%    format('Content-type: text/plain~n~n'),
+%    format('~s', [Response]).
+
+answers_quiz40_handler(Request) :-
+    http_read_data(Request, Data, []),
+    processar_corpo(Data),
+    format('Content-type: text/plain~n~n'),
+    format('Answers processed and asserted as facts.').
+
+process_and_assert_answers(InputData) :-
+    split_string(InputData, "\n", "", Lines),
+    process_lines(Lines, 1).
+
+process_lines([], _).
+process_lines([QuestionIdString, "5"|Rest], QuestionId) :-
+    atom_number(QuestionIdString, QuestionId),
+    assertz(facto(QuestionId, pergunta(QuestionId, 5))),
+    NextQuestionId is QuestionId + 1,
+    process_lines(Rest, NextQuestionId).
+
+% HTTP POST to receive answers for quiz40
+:- http_handler('/api/answersQuiz40', quiz40, [method(post)]).
+quiz40(Request) :-
+    member(method(post), Request),
+    http_read_data(Request, Data, [to(string)]),
+    processar_corpo(Data, Resultado, 1, ContadorFinal),
+    ValorUltimoFacto is ContadorFinal - 1,
+    retractall(ultimo_facto(_)), % Remove the existing ultimo_facto
+    assert(ultimo_facto(ValorUltimoFacto)), % Assert the new value
+    format('Content-type: text/plain; charset=UTF-8~n~n'),
+    format('Numero de fatos criados: ~d~n', [ValorUltimoFacto]),
+    format('~w', [Resultado]). % Saída em texto simples
+
+calcular_valor_total_sindrome(Transtorno, QuestionIds) :-
+    findall(Valor, (
+        member(QuestionId, QuestionIds),
+        facto(QuestionId, pergunta(QuestionId, Valor))
+    ), Valores),
+    sum_list(Valores, Total),
+    assertz(transtorno(Transtorno, total(Total))),
+    %write('Valor Total para '), write(Transtorno), write(': '), 
+    write(Total), nl.
+
+calcula_valores_totais :-
+    calcular_valor_total_sindrome(ansiedade_Generalizada,[1,2,3,4,5], Total1),
+    calcular_valor_total_sindrome(transtorno_de_Panico,[6,7,8,9,10], Total2),
+    calcular_valor_total_sindrome(transtorno_de_Panico_com_Agorafobia,[11,12,13,14,15], Total3),
+    calcular_valor_total_sindrome(agorafobia,[16,17,18,19,20], Total4),
+    calcular_valor_total_sindrome(ansiedade_Social,[21,22,23,24,25], Total5),
+    calcular_valor_total_sindrome(fobia_especifica,[26,27,28,29,30], Total6),
+    calcular_valor_total_sindrome(mutismo_Seletivo,[31,32,33,34,35], Total7),
+    calcular_valor_total_sindrome(ansiedade_de_separacao,[36,37,38,39,40], Total8),
+    assertz(total_calculado(ansiedade_Generalizada, Total1)),
+    assertz(total_calculado(transtorno_de_Panico, Total2)),
+    assertz(total_calculado(transtorno_de_Panico_com_Agorafobia, Total3)),
+    assertz(total_calculado(agorafobia, Total4)),
+    assertz(total_calculado(ansiedade_Social, Total5)),
+    assertz(total_calculado(fobia_especifica, Total6)),
+    assertz(total_calculado(mutismo_Seletivo, Total7)),
+    assertz(total_calculado(ansiedade_de_separacao, Total8)),
+	write('Total para ansiedade_Generalizada: '), write(Total1), nl,
+    write('Total para transtorno_de_Panico: '), write(Total2), nl,
+    write('Total para transtorno_de_Panico_com_Agorafobia: '), write(Total3), nl,
+    write('Total para agorafobia: '), write(Total4), nl,
+    write('Total para ansiedade_Social: '), write(Total5), nl,
+    write('Total para fobia_especifica: '), write(Total6), nl,
+    write('Total para mutismo_Seletivo: '), write(Total7), nl,
+    write('Total para ansiedade_de_separacao: '), write(Total8), nl.
+
+calcular_valor_total_sindrome(Transtorno, QuestionIds, Total) :-
+    findall(Valor, (
+        member(QuestionId, QuestionIds),
+        facto(QuestionId, pergunta(QuestionId, Valor))
+    ), Valores),
+    sum_list(Valores, Total).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Metodos auxiliares de manipulação dos pedidos
@@ -84,4 +166,3 @@ create_facts([[Pergunta, Resposta] | RestoLinhas], [pergunta(Pergunta, Resposta)
     assertz(facto(Contador, pergunta(PeguntaNumber, AtomResposta))),
     NewContadorFactos is Contador + 1,
     create_facts(RestoLinhas, RestoPares, NewContadorFactos, ContadorFinal).
-
